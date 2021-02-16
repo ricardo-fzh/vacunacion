ffecc6435f67ce23651d643e9b5089284f0092f1
Ricardo <ricardo.fuentes.zh@gmail.com>
2021-02-16 11:35:48 -0300

    Se añade mejoras al rut

--- app/forms.py
+++ app/forms.py
@@ -16,17 +16,24 @@ class HorasForm(forms.ModelForm):
 
     
 class PersonaForm(forms.ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super(PersonaForm, self).__init__(*args, **kwargs)
+        self.fields['dv'].label = False
+
     class  Meta:
         model =  Persona
         fields = '__all__'
         exclude = ['vacuna_disponible', 'centros','horas', 'horas_seg_v', 'fecha_seg_vacunacion']
         widgets = {
+        'email' : forms.TextInput(attrs={'placeholder': 'ejemplo@ejemplo.cl'}),
         'fecha_nac': forms.TextInput(attrs={'type': 'date'}),
         'celular': forms.TextInput(attrs={'placeholder': '87654321', 'type': 'tel', 'size':8}),
         'direccion' :forms.TextInput(attrs={'placeholder': 'Ej: Los Aromos 3339, Renca'}),
         'departamento' :forms.TextInput(attrs={'placeholder': 'Ej: 1420'}),
         'block' :forms.TextInput(attrs={'placeholder': 'Ej: Torre A'})
         }
+    
     
     #Validaciones se debe validar el rut si existe y es correcto   
 
