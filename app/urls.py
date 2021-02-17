a3424f46220f81a8206dabac28c8ab4144562ed6
ricardo-fzh <ricardo.fuentes.zh@gmail.com>
2021-02-17 02:24:26 -0300

    Se añaden las mejoras solicitadas

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('mantenedor-fechas/', mantenedor_fecha, name='mantenedor-fechas'),
     path('mantenedor-personas/', mantenedor_persona, name='mantenedor-personas'),
+    path('mantenedor-personas-admin/', mantenedor_persona_admin, name='mantenedor-personas-admin'),
     path('add-fecha/', add_fecha, name='add-fecha'),
+    path('add-fecha/<pk>/', add_fecha_admin, name='add-fecha-admin'),
     path('update-fecha/<pk>/', update_fecha, name='update-fecha'),
+    path('update-fecha/<pk>/<pk_centro>/', update_fecha_admin, name='update-fecha-admin'),
     path('delete-fecha/<pk>/', delete_fecha, name='delete-fecha'),
 ]
--- app/views.py
+++ app/views.py
@@ -216,34 +216,57 @@ def mantenedor_fecha(request):
             data = { 
                 'centros': centros,
             } 
+            if request.method == 'POST':
+                centro_id = request.POST.get('centros')
+                if centro_id != None:
+                    centro = Centro.objects.get(id=centro_id)
+                    horas = centro.horas.all()
+                    data['horas'] = horas
+                    data['centro_id'] = centro_id
             return render(request, 'app/mantenedor_fechas.html', data)
         else: 
             centro = Centro.objects.get(
                 nombre__icontains=request.user.profile.centro.nombre)
             centros = Centro.objects.all()
             horas = centro.horas.all()
-            data = {'centros': centro, 'centros_all':centros, 'horas': horas}
+            data = {'centros': centro, 'horas': horas}
             return render(request, 'app/mantenedor_fechas.html', data)
     except ObjectDoesNotExist:
         logout(request)
         messages.error(request, 'Usuario no tiene un centro asociado')
         return redirect(to='login')
 
-def update_fecha(request, pk):
+def update_fecha_admin(request, pk, pk_centro):
     if not request.user.is_authenticated:
         return redirect(to='login')
+    if request.user.is_staff:
+        centro = Centro.objects.get(pk=pk_centro)
+        hora = get_object_or_404(Hora, pk=pk)
+        form = HorasForm(instance=hora)
+        data = {
+            "horas": hora,
+            "centros": centro,
+            "form": form,
+        }
+        if request.method == 'POST':
+            form = HorasForm(data=request.POST, instance=hora)
+            if form.is_valid():
+                form.save(data)
+                return redirect(to='mantenedor-fechas')
+            else:
+                data['form'] = form
+        return render(request, 'app/update-fecha.html', data)
 
+def update_fecha(request, pk):
     centro = Centro.objects.get(
         nombre__icontains=request.user.profile.centro.nombre)
     hora = get_object_or_404(Hora, pk=pk)
     form = HorasForm(instance=hora)
-
     data = {
         "horas": hora,
         "centros": centro,
         "form": form,
     }
-
     if request.method == 'POST':
         form = HorasForm(data=request.POST, instance=hora)
         if form.is_valid():
@@ -252,6 +275,46 @@ def update_fecha(request, pk):
         else:
             data['form'] = form
     return render(request, 'app/update-fecha.html', data)
+
+def add_fecha_admin(request, pk):
+    if not request.user.is_authenticated:
+        return redirect(to='login')
+
+    if request.user.is_staff:
+        centro = Centro.objects.get(pk=pk)
+
+        if request.method == 'POST':
+            data = pd.read_csv(request.FILES['myfile'])
+            data_ext = request.FILES['myfile']
+            extension = os.path.splitext(str(data_ext))[1]
+
+            if extension != '.csv':
+                messages.error(request, 'El archivo debe ser de extensión .CSV')
+                return redirect(to="mantenedor-fechas")
+
+            horas = [
+                Hora(
+                    hora=row['hora'],
+                    dia=row['dia'],
+                    cupos=row['cupos'],
+                    created_at=date.today(),
+                    updated_at=date.today(),
+                )
+                for i, row in data.iterrows()
+            ]
+
+            try:
+                hora = Hora.objects.bulk_create(horas)
+                for h in hora:
+                    centro.horas.add(h.id)
+                messages.success(request, f"Horas cargadas exitosamente")
+                return redirect(to="mantenedor-fechas")
+            except:
+                messages.error(
+                    request, f"Error al cargar archivo, verifique la estructura del archivo")
+                return redirect(to="mantenedor-fechas")
+
+        return render(request, 'app/add-fecha.html')
 
 def add_fecha(request):
     if not request.user.is_authenticated:
@@ -299,29 +362,46 @@ def delete_fecha(request, pk):
     hora = get_object_or_404(Hora, pk=pk)
     hora.delete()
     return redirect(to='mantenedor-fechas')
-
-def mantenedor_persona(request):
+ 
+def mantenedor_persona_admin(request):
     if not request.user.is_authenticated:
         return redirect(to='login')
-
-    try:
-        if request.user.is_staff:
-            print('es staff')
-            return render(request, 'app/mantenedor_fechas.html')
-        else:
-            centros = Centro.objects.get(
-                nombre__icontains=request.user.profile.centro.nombre)
-            personas = Persona.objects.filter(centros__nombre=centros.nombre)
-
-            data = {'centros': centros, 'usuarios': personas}
-
-            if request.method == 'POST':
+    if request.user.is_staff:
+        centros_all = Centro.objects.all()
+        data = { 
+            'centros_all': centros_all,
+        } 
+        if request.method == 'POST':
+            delete = request.POST.get('del')
+ 
+            if delete == 'del':
+                centro_id = request.POST.get('centros')
                 user_id = request.POST.get('id')
+                persona = Persona.objects.get(id=user_id)
+                if persona.vacuna_disponible == 1:
+                    persona.fecha_primer_registro = None
+                    persona.vacuna_disponible = 2
+                    persona.save()
+                    messages.success(request, 'Hora anulada')
+
+                if persona.vacuna_disponible == 0:
+                    persona.fecha_seg_vacunacion = None
+                    persona.horas_seg_v = '00:00:00'
+                    persona.fecha_segundo_registro =None
+                    persona.vacuna_disponible = 1
+                    persona.save()
+                    messages.success(request, 'Hora anulada')
+            else:
+                centro_id = request.POST.get('centros')
+                user_id = request.POST.get('id')
+                if centro_id != None:
+                    centros = Centro.objects.get(id=centro_id)
+                    personas = Persona.objects.filter(centros__nombre=centros)
+                    data['centros'] = centros
+                    data['usuarios'] = personas
                 if user_id != None:
                     persona = Persona.objects.get(id=user_id)
-
                     if persona.vacuna_disponible == 2:
-
                         x = datetime.date(2021, 3, 26)
                         # date.today()
                         if date.today() >= persona.fecha_vacunacion:
@@ -354,10 +434,58 @@ def mantenedor_persona(request):
                                 request, f'Debe esperar 28 días para la proxima vacuna {end_date}')
                     else:
                         messages.error(request, 'Persona ya se encuentra vacunada')
+        return render(request, 'app/mantenedor_persona.html', data)
+
+def mantenedor_persona(request):
+    if not request.user.is_authenticated:
+        return redirect(to='login')
+
+    try:
+        centros = Centro.objects.get(
+            nombre__icontains=request.user.profile.centro.nombre)
+        personas = Persona.objects.filter(centros__nombre=centros.nombre)
+        data = {'centros': centros, 'usuarios': personas}
+        if request.method == 'POST':
+            user_id = request.POST.get('id')
+            if user_id != None:
+                persona = Persona.objects.get(id=user_id)
+                if persona.vacuna_disponible == 2:
+                    x = datetime.date(2021, 3, 26)
+                    # date.today()
+                    if date.today() >= persona.fecha_vacunacion:
+                        persona.vacuna_disponible = persona.vacuna_disponible - 1
+                        persona.fecha_primer_registro = date.today()
+                        persona.save()
+                        messages.success(
+                            request, 'Persona registrada en el sistema')
+                    else:
+                        messages.error(
+                            request, f'Debe esperar 28 días para la proxima vacuna')
+                elif persona.vacuna_disponible == 1:
+                    start_date = persona.fecha_primer_registro.strftime(
+                        "%Y-%m-%d")
+                    date_1 = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+                    end_date = date_1 + datetime.timedelta(days=28)
+                    x = datetime.datetime(2021, 3, 23)
+                    # datetime.datetime.today()
+                    if datetime.datetime.today() >= end_date:
+                        persona.vacuna_disponible = persona.vacuna_disponible - 1
+                        persona.fecha_vacunacion = persona.fecha_vacunacion
+                        persona.fecha_primer_registro = persona.fecha_primer_registro
+                        persona.fecha_seg_vacunacion = persona.fecha_seg_vacunacion
+                        persona.fecha_segundo_registro = date.today()
+                        persona.save()
+                        messages.success(
+                            request, 'Persona registrada en el sistema')
+                    else:
+                        messages.error(
+                            request, f'Debe esperar 28 días para la proxima vacuna {end_date}')
                 else:
-                    messages.error(
-                        request, 'Persona no se encuentra registrada sistema')
-            return render(request, 'app/mantenedor_persona.html', data)
+                    messages.error(request, 'Persona ya se encuentra vacunada')
+            else:
+                messages.error(
+                    request, 'Persona no se encuentra registrada sistema')
+        return render(request, 'app/mantenedor_persona.html', data)
     except ObjectDoesNotExist:
         logout(request)
         messages.error(request, 'Usuario no tiene un centro asociado')
